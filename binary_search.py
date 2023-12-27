806a829121baa5e71470c41f08ab4ee1fe26f4df
Boney Patel <boneypatel37@yahoo.com>
2023-12-27 15:45:14 -0500

    Complete Leetcode Easy Binary Search

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def search(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        m = (l + r) // 2
+        if nums[m] < target:
+            l = m + 1
+        elif nums[m] > target:
+            r = m - 1
+        else:
+            return m
+
+    return -1
+
+assert search([-1, 0, 3, 5, 9, 12], 9) == 4
